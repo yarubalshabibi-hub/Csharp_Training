524de035809bddb109743d5bee5462b2617a0d41
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-05-12 10:42:31 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
